81046f902dc0bfbc8eeca89f44569b57a4eda903
shuli495 <xiaoyao495@163.com>
2023-05-12 00:52:41 +0800

    update HttpExchangeRegister 查找package逻辑

--- src/main/java/com/pighand/framework/spring/http/exchange/HttpExchangeRegister.java
+++ src/main/java/com/pighand/framework/spring/http/exchange/HttpExchangeRegister.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -24,7 +27,37 @@ import java.util.Set;
  *
  * @author wangshuli
  */
-public class HttpExchangeRegister implements BeanDefinitionRegistryPostProcessor {
+public class HttpExchangeRegister
+        implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+    /**
+     * 查找启动类包名
+     *
+     * @return
+     */
+    private String findApplicationPackageName() {
+        ConfigurableListableBeanFactory beanFactory =
+                (ConfigurableListableBeanFactory)
+                        applicationContext.getAutowireCapableBeanFactory();
+
+        String[] beanNames =
+                beanFactory.getBeanNamesForAnnotation(
+                        org.springframework.boot.autoconfigure.SpringBootApplication.class);
+
+        if (beanNames.length > 0) {
+            String beanName = beanNames[0];
+            String className = beanFactory.getBeanDefinition(beanName).getBeanClassName();
+            return ClassUtils.getPackageName(className);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     /**
      * 扫描带有@HttpExchange的接口，使用默认WebClient生成代理，以驼峰命名方式注入到spring容器
@@ -39,7 +72,11 @@ public class HttpExchangeRegister implements BeanDefinitionRegistryPostProcessor
                 new ClassPathScanningCandidateInterfaceProvider();
         provider.addIncludeFilter(new AnnotationTypeFilter(HttpExchange.class));
 
-        String packageName = getClass().getClassLoader().getDefinedPackages()[0].getName();
+        String packageName = findApplicationPackageName();
+
+        if (packageName == null) {
+            throw new RuntimeException("can not find application package name");
+        }
 
         Set<BeanDefinition> httpExchangeBeans = provider.findCandidateComponents(packageName);
 
