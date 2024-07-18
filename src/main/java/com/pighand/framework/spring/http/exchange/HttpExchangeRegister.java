fd21da1955c60170aeffc2ba5447daf55e28b683
shuli495 <xiaoyao495@163.com>
2024-07-18 11:45:07 +0800

    update httpExchange注入兼容spring6.2

--- src/main/java/com/pighand/framework/spring/http/exchange/HttpExchangeRegister.java
+++ src/main/java/com/pighand/framework/spring/http/exchange/HttpExchangeRegister.java
@@ -10,14 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.util.ClassUtils;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
-import reactor.netty.http.client.HttpClient;
 
 import java.util.Set;
 
@@ -28,8 +25,7 @@ import java.util.Set;
  *
  * @author wangshuli
  */
-public class HttpExchangeRegister
-        implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
+public class HttpExchangeRegister implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
     /**
@@ -39,8 +35,7 @@ public class HttpExchangeRegister
      */
     private String findApplicationPackageName() {
         ConfigurableListableBeanFactory beanFactory =
-                (ConfigurableListableBeanFactory)
-                        applicationContext.getAutowireCapableBeanFactory();
+            (ConfigurableListableBeanFactory)applicationContext.getAutowireCapableBeanFactory();
 
         String[] beanNames = beanFactory.getBeanNamesForAnnotation(SpringBootApplication.class);
 
@@ -65,10 +60,8 @@ public class HttpExchangeRegister
      * @throws BeansException
      */
     @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
-            throws BeansException {
-        ClassPathScanningCandidateInterfaceProvider provider =
-                new ClassPathScanningCandidateInterfaceProvider();
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        ClassPathScanningCandidateInterfaceProvider provider = new ClassPathScanningCandidateInterfaceProvider();
         provider.addIncludeFilter(new AnnotationTypeFilter(HttpExchange.class));
 
         String packageName = findApplicationPackageName();
@@ -80,13 +73,6 @@ public class HttpExchangeRegister
         Set<BeanDefinition> httpExchangeBeans = provider.findCandidateComponents(packageName);
 
         for (BeanDefinition httpExchangeBean : httpExchangeBeans) {
-
-            ReactorClientHttpConnector connector =
-                    new ReactorClientHttpConnector(
-                            HttpClient.create().wiretap(true).compress(true));
-
-            WebClient webClient = WebClient.builder().clientConnector(connector).build();
-
             Class clz = null;
             try {
                 clz = Class.forName(httpExchangeBean.getBeanClassName());
@@ -94,11 +80,10 @@ public class HttpExchangeRegister
                 throw new RuntimeException(e);
             }
 
+            RestClient client = RestClient.create();
+
             Object bean =
-                    HttpServiceProxyFactory.builder()
-                            .clientAdapter(WebClientAdapter.forClient(webClient))
-                            .build()
-                            .createClient(clz);
+                HttpServiceProxyFactory.builderFor(RestClientAdapter.create(client)).build().createClient(clz);
 
             RootBeanDefinition beanDefinition = new RootBeanDefinition(clz, () -> bean);
 
@@ -112,6 +97,6 @@ public class HttpExchangeRegister
     }
 
     @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
-            throws BeansException {}
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    }
 }
